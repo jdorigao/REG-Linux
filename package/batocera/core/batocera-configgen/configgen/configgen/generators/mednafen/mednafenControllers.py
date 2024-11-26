44dad411cff713d26d7da5c1ba9996002823e0d6
Juliano Dorigão <jdorigao@gmail.com>
2024-11-26 09:03:52 -0400

    WIP: mednafenGenerator

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/mednafen/mednafenControllers.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/mednafen/mednafenControllers.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
 
-from utils.logger import get_logger
+psxJoystick = {
+    'a' :             {'button': 'Circle'},
+    'b' :             {'button': 'Cross'},
+    'x' :             {'button': 'Triangle'},
+    'y' :             {'button': 'Square'},
+    'start' :         {'button': 'Start'},
+    'select' :        {'button': 'Select'},
+    'pageup' :        {'button': 'L'},
+    'pagedown' :      {'button': 'R'},
+    'joystick1left' : {'axis': 'An.Left'},
+    'joystick1up' :   {'axis': 'An.Up'},
+    'joystick2left' : {'axis': 'RightAn.Left'},
+    'joystick2up' :   {'axis': 'RightAn.Up'},
+    'up' :            {'hat': 'Up',    'axis': 'Up',    'button': 'Up'},
+    'down' :          {'hat': 'Down',  'axis': 'Down',  'button': 'Down'},
+    'left' :          {'hat': 'Left',  'axis': 'Left',  'button': 'Left'},
+    'right' :         {'hat': 'Right', 'axis': 'Right', 'button': 'Right'},
+    'joystick1right' :{'axis': 'An.Right'},
+    'joystick1down' : {'axis': 'An.Down'},
+    'joystick2right' :{'axis': 'RightAn.Right'},
+    'joystick2down' : {'axis': 'RightAn.Down'}
+}
 
-eslog = get_logger(__name__)
+# md.input.port1.gamepad6.y joystick 0x0003054c026881100006001100000000 button_4
 
-def setMednafenControllers(cfgConfig):
-    cfgConfig.write("\n")
+def generateControllerConfig(cfgConfig, mednafenSystem, controller):
+
+    for system in mednafenSystem:
+        # Input device for Virtual Port 1
+        cfgConfig.write(system + ".input.port" + str(controller.index) + ".gamepad6." + "\n")
+
