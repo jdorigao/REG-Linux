f8dc689003c49766c87ebf8b1ebec613b1d73147
Juliano Dorigão <jdorigao@gmail.com>
2025-02-26 15:53:32 -0400

    labwc

---
+++ package/reglinux/utils/labwc/labwc.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# Labwc
+#
+################################################################################
+
+LABWC_VERSION = 0.8.3
+LABWC_SITE = $(call github,labwc,labwc,$(LABWC_VERSION))
+LABWC_LICENSE = BSD 3-Clause
+LABWC_LICENSE_FILES = LICENSE
+LABWC_DEPENDENCIES = wlroots libxml2 libpng cairo pango
+
+LABWC_CONF_OPTS = \
+	-Dman-pages=disabled \
+	-Dsvg=disabled \
+	-Dicon=disabled
+
+$(eval $(meson-package))
