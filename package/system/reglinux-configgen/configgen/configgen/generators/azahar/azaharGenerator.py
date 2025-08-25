cc1e23b48c334dbe60d332c8080e47fa0e302faa
Juliano Dorigão <jdorigao@gmail.com>
2025-08-24 20:49:07 -0400

    configgen: azahar (UnixSettings)

--- package/system/reglinux-configgen/configgen/configgen/generators/azahar/azaharGenerator.py
+++ package/system/reglinux-configgen/configgen/configgen/generators/azahar/azaharGenerator.py
@@ -1,8 +1,6 @@
 from generators.Generator import Generator
 from Command import Command
-from configparser import RawConfigParser
-from os import path, makedirs
-from .azaharConfig import AZAHAR_BIN_PATH, AZAHAR_CONFIG_PATH, setAzaharConfig
+from .azaharConfig import AZAHAR_BIN_PATH, setAzaharConfig
 from .azaharControllers import setAzaharControllers
 
 from utils.logger import get_logger
@@ -11,20 +9,9 @@ eslog = get_logger(__name__)
 class AzaharGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-
-        azaharConfig = RawConfigParser(strict=False)
-        azaharConfig.optionxform=lambda optionstr: str(optionstr)
-
-        if path.exists(AZAHAR_CONFIG_PATH):
-            azaharConfig.read(AZAHAR_CONFIG_PATH)
-
-        setAzaharConfig(azaharConfig, system)
+        azaharConfig = setAzaharConfig(system)
         setAzaharControllers(azaharConfig, playersControllers)
-
-        if not path.exists(path.dirname(AZAHAR_CONFIG_PATH)):
-            makedirs(path.dirname(AZAHAR_CONFIG_PATH))
-        with open(AZAHAR_CONFIG_PATH, 'w') as configfile:
-            azaharConfig.write(configfile)
+        azaharConfig.write()  # UnixSettings method
 
         commandArray = [AZAHAR_BIN_PATH, rom]
         return Command(array=commandArray)
